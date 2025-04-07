1b78413cdcb497f934c315279924a0e443030681
Matej Belobrad <271286@vutbr.cz>
2025-04-07 09:01:34 +0200

    first commit

--- searching.py
+++ searching.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -12,10 +12,17 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    allowed_keyz = {"unordered_numbers","ordered_numbers","dna_sequence"}
+    if field not in allowed_keyz:
+        return None
+
+    with open(file_path, mode="r") as f:
+        data=json.load(f)
 
 
+    return data[field]
 def main():
-    pass
+    print(read_data("sequential.json", "unordered_numbers"))
 
 
 if __name__ == '__main__':
